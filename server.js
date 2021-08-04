903435dcda3b904acc1e29fd5251cd9b48c0951c
fguajard <franciscogm31@gmail.com>
2021-08-04 10:16:18 -0400

    agregado cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const usersRouter = require("./routes/users");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
